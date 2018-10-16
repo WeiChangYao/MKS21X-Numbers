a0361269e52981387c48de9a78d675336357c51e
weichanhyao <wyao1@stuy.edu>
2018-10-16 00:15:32 -0400

    added the two things

---
+++ RationalNumber.java
@@ -0,0 +1,10 @@
+public class RationalNumber extends RealNumber{
+  private int denominator;
+  private int numerator;
+  public RationalNumber(int a, int b){
+    super(0.0);
+  }
+  public int compareRatRat(RationalNumber a, RationalNumber b){
+    return 1;
+  }
+}
---
+++ RealNumber.java
@@ -0,0 +1,16 @@
+public class RealNumber{
+  private double realN;
+  public RealNumber(double n){
+    realN = n;
+  }
+  public double getNum(){
+    return realN;
+  }
+  public String toString(){
+    return "";
+  }
+  //returns larger value
+  public double compareRealReal(RealNumber a, RealNumber b){
+    return 1.0;
+  }
+}
